db3787c95752d172d069bb097b474dfbd2454869
cadop <cadop@users.noreply.github.com>
2023-08-17 17:45:28 -0400

    cleanup usd utils

--- usd_utils.py
+++ usd_utils.py
@@ -17,65 +17,23 @@ def get_all_stage_mesh(stage):
         if x.IsA(UsdGeom.Mesh):
             found_meshes.append(x)
 
-    points, faces = get_mesh(stage, found_meshes)
-    # points, faces = get_mesh(stage, found_meshes[:200])
-    
+    points, faces = get_mesh(found_meshes)
+   
     return points, faces
 
 
-def get_mesh(usd_stage, objs):
+def get_mesh(objs):
 
     points, faces = [],[]
 
     for obj in objs:
         f_offset = len(points)
         f, p = meshconvert(obj)
-        # f, p = convert_to_mesh(obj, unit_scale=0.01)
-        # f, p = convert_to_mesh(usd_stage.GetPrimAtPath(obj))
         if len(p) == 0: continue
         points.extend(p)
         faces.extend(f+f_offset)
 
     return points, faces
-
-def convert_to_mesh(prim, unit_scale=1):
-    ''' convert a prim to BVH '''
-
-    # Get mesh name (prim name)
-    m = UsdGeom.Mesh(prim)
-
-    # Get verts and triangles
-    tris = m.GetFaceVertexIndicesAttr().Get()
-
-    if not tris:
-        return [], []
-
-    tris_cnt = m.GetFaceVertexCountsAttr().Get()
-
-    verts = m.GetPointsAttr().Get()
-
-    tri_list = np.array(tris)
-    vert_list = np.array(verts)
-
-    # xform = UsdGeom.Xformable(prim)
-    # time = Usd.TimeCode.Default() # The time at which we compute the bounding box
-    # world_transform: Gf.Matrix4d = xform.ComputeLocalToWorldTransform(time)
-    # translation: Gf.Vec3d = world_transform.ExtractTranslation()
-    # rotation: Gf.Rotation = world_transform.ExtractRotationMatrix()
-    # # rotation: Gf.Rotation = world_transform.ExtractRotation()
-    # scale: Gf.Vec3d = Gf.Vec3d(*(v.GetLength() for v in world_transform.ExtractRotationMatrix()))
-    # rotation = rotation.GetOrthonormalized()
-
-    # # New vertices
-    # vert_list = np.dot((vert_list * (scale * unit_scale) ), rotation) + translation
-
-    tri_list = convert_to_triangle_mesh(tris, tris_cnt)
-    # faces = parse_faces(tris, tris_cnt)
-    # tri_list = convert_to_triangle_mesh(faces)
-    tri_list = tri_list.flatten()
-    # tri_list = triangulate(tris_cnt, tris)
-
-    return tri_list, vert_list
 
 def meshconvert(prim):
 
@@ -90,7 +48,6 @@ def meshconvert(prim):
     if not tris:
         return [], []
     tris_cnt = mesh.GetFaceVertexCountsAttr().Get()
-
 
     # Get the vertices in local space
     points_attr = mesh.GetPointsAttr()
@@ -113,40 +70,12 @@ def meshconvert(prim):
     # Transform all vertices to world space using matrix multiplication
     world_points = np.dot(points_np, matrix_np)
 
-    # Convert back to 3D coordinates (if needed)
-    # world_points[:, :3] /= world_points[:, 3, np.newaxis]
-
     tri_list = convert_to_triangle_mesh(tris, tris_cnt)
     tri_list = tri_list.flatten()
 
     world_points = world_points[:,:3]
 
     return tri_list, world_points
-
-
-
-def triangulate(face_counts, face_indices):
-    '''
-    Taken from :
-    https://github.com/NVIDIA/warp/blob/main/warp/tests/test_mesh_query_point.py#L248
-    '''
-
-    num_tris = np.sum(np.subtract(face_counts, 2))
-    num_tri_vtx = num_tris * 3
-    tri_indices = np.zeros(num_tri_vtx, dtype=int)
-    ctr = 0
-    wedgeIdx = 0
-
-    for nb in face_counts:
-        for i in range(nb - 2):
-            tri_indices[ctr] = face_indices[wedgeIdx]
-            tri_indices[ctr + 1] = face_indices[wedgeIdx + i + 1]
-            tri_indices[ctr + 2] = face_indices[wedgeIdx + i + 2]
-            ctr += 3
-        wedgeIdx += nb
-
-    return tri_indices
-
 
 def convert_to_triangle_mesh(FaceVertexIndices, FaceVertexCounts):
     """
