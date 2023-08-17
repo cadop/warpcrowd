1f0e5f5e967e02d5d636e11699bc7ca0c33d83a5
cadop <cadop@users.noreply.github.com>
2023-08-17 17:42:16 -0400

    only render mesh on first frame

--- run_usd_ex.py
+++ run_usd_ex.py
@@ -13,19 +13,14 @@ wp.init()
 def run_class():
 
     # USD Scene stuff
-    usd_stage = Usd.Stage.Open(os.path.join(os.path.dirname(__file__), "simple_env.usd"))
+    usd_stage = Usd.Stage.Open(os.path.join(os.path.dirname(__file__), "Collected_simple_env2/simple_env2.usd"))
     
-    parent_prim = usd_stage.GetPrimAtPath('/World')
-    points, faces = usd_utils.children_as_mesh(usd_stage, parent_prim)
-
+    points, faces = usd_utils.get_all_stage_mesh(usd_stage)
     stage = os.path.join(os.path.dirname(__file__), "warpcrowd_output.usd")
-    renderer = render_usd.UsdRenderer(stage, up_axis='y')
+    renderer = render_usd.UsdRenderer(stage, up_axis='z')
 
-    wc = WarpCrowd()
-    wc.demo_agents(m=20, n=20)
-
-
-    # faces = usd_utils.triangulate(faces, points)
+    wc = WarpCrowd(up_axis='z')
+    wc.demo_agents(m=3, n=3)
 
     wc.config_hashgrid()
     wc.config_mesh(np.asarray(points), np.asarray(faces))
@@ -35,9 +30,16 @@ def run_class():
 
     start = time.time()
     sim_time = 0
-    for i in range(700):
+
+    # Render an initial frame to insert static mesh
+    renderer.begin_frame(0.0)
+    renderer.render_mesh(name="mesh", points=wc.mesh.points.numpy(), indices=wc.mesh.indices.numpy())
+    renderer.end_frame()
+    
+    for i in range(1):
         wc.compute_step()
         sim_time = render(False, renderer, wc.mesh, wc.xnew_wp, radius, sim_time, wc.dt)
+    
     print(time.time()-start)
     renderer.save()
 
@@ -47,23 +49,7 @@ def render(is_live, renderer, mesh, positions, sim_margin, sim_time, dt):
         time = 0.0 if is_live else sim_time 
         
         renderer.begin_frame(time)
-
-        # box_path = renderer.root.GetPath().AppendChild('boxes')
-        # box = UsdGeom.Cube.Get(renderer.stage, box_path)
-        # if not box:
-        #     box = UsdGeom.Cube.Define(renderer.stage, box_path)
-        #     renderer._usd_add_xform(box)
-
-        # xform = UsdGeom.Xform(xform)
-        # xform_ops = xform.GetOrderedXformOps()
-
-        # xform_ops[0].Set(Gf.Vec3d(float(pos[0]), float(pos[1]), float(pos[2])), time)
-        # xform_ops[1].Set(Gf.Quatf(float(rot[3]), float(rot[0]), float(rot[1]), float(rot[2])), time)
-        # xform_ops[2].Set(Gf.Vec3d(float(scale[0]), float(scale[1]), float(scale[2])), time)
-
-        renderer.render_mesh(name="mesh", points=mesh.points.numpy(), indices=mesh.indices.numpy())
         renderer.render_points(name="points", points=positions.numpy(), radius=sim_margin)
-        # render_box(self, name: str, pos: tuple, rot: tuple, extents: tuple)
         renderer.end_frame()
 
     sim_time += dt
